1cb5b70564829ea235936a2cc1a14340d30465fa
hamzaarbeed <hamza.arbeed@gmail.com>
2018-11-28 09:47:09 -0600

    Initial commit

---
+++ app/src/main/java/com/test/birds/MainActivity.java
@@ -0,0 +1,68 @@
+package com.test.birds;
+
+import android.content.Context;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private Context context = this;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button calc = (Button) findViewById(R.id.button);
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> mDataset = new ArrayList<String>();
+
+                int TN = Integer.parseInt(((TextInputEditText)findViewById(R.id.totalBirds)).getText().toString());
+
+                int TP = Integer.parseInt(((TextInputEditText)findViewById(R.id.totalPrice)).getText().toString());
+                double P1 = Double.parseDouble(((TextInputEditText)findViewById(R.id.fBirdPrice)).getText().toString());
+                double P2 = Double.parseDouble(((TextInputEditText)findViewById(R.id.sBirdPrice)).getText().toString());
+                double P3 = Double.parseDouble(((TextInputEditText)findViewById(R.id.tBirdPrice)).getText().toString());
+                Log.v("Birds", "error5.5");
+                double N1, N2, N3;
+                for (N3 = 1; N3 * P3 < TP && N3 < TN; N3++) {
+                    N2 = (TP - TN * P1 - N3 * (P3 - P1)) / (P2 - P1);
+                    N1 = TN - N2 - N3;
+                    if (N2 % 1 == 0 && N2 > 0 && N1 % 1 == 0 && N1 > 0)
+                        mDataset.add((int) N1 + " ," + (int) N2 + " ," + (int) N3);
+                }
+                if (mDataset.size() == 0)
+                    mDataset.add("There is no result");
+                mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+                // use this setting to improve performance if you know that changes
+                // in content do not change the layout size of the RecyclerView
+                Log.v("Birds", "error7");
+                // use a linear layout manager
+                mLayoutManager = new LinearLayoutManager(context);
+                mRecyclerView.setLayoutManager(mLayoutManager);
+
+                // specify an adapter (see also next example)
+                Log.v("Birds", "error");
+                mAdapter = new MyAdapter(context,mDataset);
+                Log.v("Birds", "error2");
+                mRecyclerView.setAdapter(mAdapter);
+                Log.v("Birds", "error3");
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/test/birds/MyAdapter.java
@@ -0,0 +1,55 @@
+package com.test.birds;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private ArrayList<String> mData=new ArrayList<String>();
+    private LayoutInflater mInflater;
+
+    // data is passed into the constructor
+    MyAdapter(Context context, ArrayList<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.item_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String animal = mData.get(position);
+        holder.myTextView.setText(animal);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.textView);
+        }
+
+    }
+
+}
